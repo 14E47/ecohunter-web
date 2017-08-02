b4cd3eff05413db435ab4ded2ee601c9d7cc4370
rohitshrma <er.rohitshrma@yahoo.com>
2017-08-02 11:12:14 +0530

    top products added

--- custom_oscar/catalogue/templatetags/customtag.py
+++ custom_oscar/catalogue/templatetags/customtag.py
@@ -4,3 +4,10 @@ from booking.models import Booking
 import pdb
 
 register = template.Library()
+
+from oscar.apps.catalogue.models import Product
+
+@register.assignment_tag
+def top_products():
+	top_products = Product.objects.filter(parent__isnull=True).order_by('-date_updated')[:6]
+	return top_products
