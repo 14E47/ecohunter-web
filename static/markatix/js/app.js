be1092c5c788480913999a638d4da5db666961ab
TheSassyGourmet <abhanavis172@gmail.com>
2017-08-15 22:45:31 +0400

    Kuchh changes

--- static/markatix/js/app.js
+++ static/markatix/js/app.js
@@ -56,7 +56,43 @@ $(document).ready(function() {
         var itemlength = $('#myCarousel .item').length;
         var triggerheight = Math.round(boxheight / itemlength + 1);
         $('#myCarousel .list-group-item').outerHeight(triggerheight);
-    })
+    });
+    // Select all links with hashes
+    $('a[href*="#"]')
+    // Remove links that don't actually link to anything
+    .not('[href="#"]')
+    .not('[href="#0"]')
+    .click(function(event) {
+        // On-page links
+        if (
+        location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
+        && 
+        location.hostname == this.hostname
+        ) {
+        // Figure out element to scroll to
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+        // Does a scroll target exist?
+        if (target.length) {
+            // Only prevent default if animation is actually gonna happen
+            event.preventDefault();
+            $('html, body').animate({
+            scrollTop: target.offset().top - (3*$('.header').height())
+            }, 700, function() {
+            // Callback after animation
+            // Must change focus!
+            var $target = $(target);
+            $target.focus();
+            if ($target.is(":focus")) { // Checking if the target was focused
+                return false;
+            } else {
+                $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+                $target.focus(); // Set focus again
+            };
+            });
+        }
+        }
+    });
 });
 
 
@@ -115,4 +151,5 @@ var checkout = $('#dpd2').datepicker({
 }).on('changeDate', function(ev) {
   checkout.hide();
 }).data('datepicker');
+
 
