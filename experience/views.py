5bb14194018861d8ab5871dccb8748851e684f56
mohan kumar <mohankumar718@gmail.com>
2017-08-17 19:39:54 +0530

    extra code deleted

--- experience/views.py
+++ experience/views.py
@@ -2,10 +2,8 @@ from django.shortcuts import render, get_object_or_404
 from experience.models import Experience
 
 def experience_list(request, template = 'experience/detail.html'):
-    n = 5
     experiences = Experience.objects.all()
-    experience_f = [experiences[i:i + n] for i in range(0, len(experiences), n)]
-    return render(request, template, {'experiences': experience_f,})
+    return render(request, template, {'experiences': experiences,})
 
 def experience_detail(request, slug, template='experience/experience.html'):
     experience = Experience.objects.all()
